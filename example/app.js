b6e0645fa6f3cdc262f70f3d772f42642ea82b1b
netpi <netpi@foxmail.com>
2014-11-16 17:49:08 +0800

    add example

---
+++ example/app.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var ejs = require('ejs');
+var path = require('path');
+var app = express();
+
+var ueditor = require("ueditor");
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+// view engine setup
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.html', ejs.__express);
+app.set('view engine', 'html');
+
+app.use("/ueditor/ue", ueditor(path.join(__dirname, 'public'), function(req, res, next) {
+    // ueditor 客户发起上传图片请求
+    console.log('ueditor')
+    // console.log(req.query)
+    if (req.query.action === 'uploadimage') {
+        var foo = req.ueditor;
+        var date = new Date();
+        var imgname = req.ueditor.filename;
+
+        var img_url = '/images/ueditor/' + date.getTime() + imgname;
+        res.ue_up(img_url); //你只要输入要保存的地址 。保存操作交给ueditor来做
+    }
+    //  客户端发起图片列表请求
+    else if (req.query.action === 'listimage') {
+        var dir_url = '/images/ueditor/';
+        res.ue_list(dir_url); // 客户端会列出 dir_url 目录下的所有图片
+    }
+    // 客户端发起其它请求
+    else {
+        // console.log('config.json')
+        res.setHeader('Content-Type', 'application/json');
+        res.redirect('/ueditor/nodejs/config.json');
+    }
+}));
+
+
+
+
+app.use('/', function(req, res) {
+    res.render('ueditor');
+});
+
+app.listen(3000, function() {
+    console.log('app listen : 3000');
+});
+
+module.exports = app;
